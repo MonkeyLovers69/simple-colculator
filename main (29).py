7643714644ab827edbe1d84b0712cab1c363b8ba
MonkeyLovers69 <137757068+MonkeyLovers69@users.noreply.github.com>
2023-07-26 14:53:20 +0500

    Add files via upload

---
+++ main (29).py
@@ -0,0 +1,11 @@
+num = float(input())
+num2 = float(input())
+symbol = input()
+if symbol == '*':
+    print( num * num2 )
+elif symbol == '/':
+    print( num / num2 )
+elif symbol == '+':
+    print( num + num2 )
+else :
+    print( num - num2 )
